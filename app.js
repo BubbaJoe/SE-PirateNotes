66a0a0c918dc8e9c54910c299d42375f04e88c9a
Joe Williams <joewilliamsis@live.com>
2017-10-20 16:12:50 -0400

    update

--- app.js
+++ app.js
@@ -41,5 +41,5 @@ var db = mysql.createConnection({
 	password: "123456"
 });
 
-require('./app/routes.js')(app, io, passport);
 require('./app/database.js')(db,fs);
+require('./app/routes.js')(app, io, passport);
--- app/database.js
+++ app/database.js
@@ -1,3 +1,25 @@
 module.exports = function(db,fs) {
-    db.connection()
+
+    function runFileQuery(fileName) {
+        db.connect(function(err) {
+            if(err) throw err;
+            fs.readFile('./sql/'+filePath,
+            'utf8',function(err,data) {
+                if(err) throw err;
+                db.query(data, function(err,result) {
+                    return result;
+                });
+            });
+        });
+    }
+
+    function runQuery(query) {
+        db.connect(function(err) {
+            if(err) throw err;
+            db.query(data, function(err, result) {
+                if(err) throw err;
+                return result;
+            });
+        });
+    }
 }
