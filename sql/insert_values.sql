d9ad1bd70e0f6a8bcc79657e042d055f6f299622
Joe Williams <joewilliamsis@live.com>
2017-11-06 13:58:26 -0500

    update

--- sql/insert_values.sql
+++ sql/insert_values.sql
@@ -1,2 +1,14 @@
-insert into user () 
-    values();
+#https://www.guidgenerator.com/online-guid-generator.aspx
+#use the link above to generate uuid/guid
+
+#insert into user (id,email,password,firstname,lastname,acc_type,acc_status)
+#    values('id','email','password','firstname','lastname','acc_type','acc_status');
+
+#insert into department (id,dept_name,dept_abbr) 
+#   values('id','dept_name','dept_abbr');
+
+#insert into course (id,dept_abbr,course_name,course_num) 
+#   values('id','dept_abbr','course_name','course_num');
+
+#insert into post (id,user_id,course_id,post_text,posdt_status) 
+#   values('id','user_id','course_id','post_text','post_status');
