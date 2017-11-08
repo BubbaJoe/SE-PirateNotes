a6d9d2dcd1fa3c177ccbfedb6ed849281bc0626d
Ozayr Abrar Shareef <shareefo16@students.ecu.edu>
2017-11-08 17:08:25 -0500

    Updated Insert Tuples

--- sql/insert_values.sql
+++ sql/insert_values.sql
@@ -25,21 +25,29 @@ values ('2a771e780a56426ca9dd1f8f1f7c0bed', '' , 'password' , 'Sam', 'Twaiti', '
 #   values('id','dept_name','dept_abbr');
 insert into department (id, dept_name, dept_abbr)
 values ('6dae5f4c4bbe49f4ba4fd5312d61020a', 'Computer Science', 'CSCI');
-
 insert into department (id, dept_name, dept_abbr)
 values ('26ad98fd69524a9e98a2c1a17297b252', 'Information Computer Technology', 'ICTN');
-
 insert into department (id, dept_name, dept_abbr)
 values ('7d4dd624d51647beb5b9023573f0d0fe', 'Management Information Systems', 'MIS');
+insert into department (id, dept_name, dept_abbr)
+values ('45457b5021de4c878019514e70ef21bc', 'Electrical Engineering', 'EENG');
+insert into department (id, dept_name, dept_abbr)
+values ('95b6c1d19aaf4219a83b417965ccf7d3', 'Industrial Technology', 'ITEC');
+insert into department (id, dept_name, dept_abbr)
+values ('796d2c23347449fa96e8e77a7d4e11d3', 'Biomedical Engineering', 'BIME');
 #insert into course (id,dept_abbr,course_name,course_num) 
 #   values('id','dept_abbr','course_name','course_num');
 insert into course (id, dept_abbr, course_name, course_num)
 values ('42ae3034b4284f87af2539597aa4d614', 'CSCI', 'Database Management System', '3700');
-
 insert into course (id, dept_abbr, course_name, course_num)
 values ('33897a56bee3475bbef1682325327149', 'ICTN', 'Digital Communications', '2154');
-
 insert into course (id, dept_abbr, course_name, course_num)
-values ('e11227b2f84d4175a16abbc2cf2ee3df', 'MIS', '', '');
+values ('e11227b2f84d4175a16abbc2cf2ee3df', 'MIS', 'Intro Management Info Systems', '3063');
+insert into course (id, dept_abbr, course_name, course_num)
+values ('4b130312720747d9996f3122f0d7ea56', 'EENG', 'Signals and Systems', '3023');
+insert into course (id, dept_abbr, course_name, course_num)
+values ('22133f551c6d4d8cbd503ac1b84850bf', 'ITEC', 'Thermal and Fluid Systems', '2081');
+insert into course (id, dept_abbr, course_name, course_num)
+values ('c980c0d7d0824b7a8ca8b1a57eb52dfd', 'BIME', 'Biomechanics and Materials', '4030');
 #insert into post (id,user_id,course_id,post_text,posdt_status) 
 #   values('id','user_id','course_id','post_text','post_status');
